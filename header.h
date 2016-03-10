c3d74385278d31da65c450c24d4c53dc207ae42f
UFO1969 <mrsht.masa@gmail.com>
2016-03-11 04:17:15 +0900

    destructive changes

--- header.h
+++ header.h
@@ -7,6 +7,7 @@
 #include <vector>
 #include <memory>
 #include <list>
+#include <unordered_map>
 
 typedef std::pair<unsigned int, unsigned int> Location;
 
@@ -16,63 +17,58 @@ std::ostream& operator<<(std::ostream& s, const std::pair<T1, T2>& p)
 	return s << "(" << p.first << "," << p.second << ")";
 }
 
+template<typename T> void hash_combine(size_t& seed, T const& v){ seed ^= std::hash<T>{}(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2); }
+
+struct pair_hash
+{
+	template<class T1, class T2> std::size_t operator()(const std::pair<T1, T2>& p) const
+	{
+		std::size_t seed = 0;
+		hash_combine(seed, p.first);
+		hash_combine(seed, p.second);
+		return seed;
+	}
+};
+
 inline unsigned int ManhattanDistance(const Location& start, const Location& dist)
 {
 	return static_cast<unsigned int>(std::abs(static_cast<int>(start.first) - static_cast<int>(dist.first)) + std::abs(static_cast<int>(start.second) - static_cast<int>(dist.second)));
 }
 
-class passenger { // 客
-private:
-	const unsigned int ID;
-	Location current_location; // 現在地
-	const Location destination; // 目的地
-	unsigned int waiting_time;
-	bool riding;
+// 出発地と目的地、またバスの路線図から、最適な乗り換え経路を算出(todo)
+std::vector<Location> GenTransferRoute(const Location& start, const Location& dest, const std::unordered_map<unsigned int, std::vector<Location>>& routes)
+{
+	return std::vector<Location>(1, dest);
+}
 
-public:
-	passenger(unsigned int IDnum, const Location& curr, const Location& dest): ID(IDnum), current_location(curr), destination(dest), waiting_time(0), riding(false){}
-
-	bool update_location(const Location& next_location)
-	{
-		// 1-ノルムで1より多く進んでいたらエラー
-        if (ManhattanDistance(current_location, next_location) > 1) { std::cout<< "fuckin' moving " << current_location << "->" << next_location << std::endl; return false; }
-		current_location = next_location;
-		return true;
-	}
-
-	bool is_arrived() const { return current_location == destination; }
-	bool is_riding() const { return riding; }
-	
-	void ride() { riding = true; return; }
-	void waiting() { waiting_time++; return; }
-	unsigned int get_waiting_time() const { return waiting_time; }
-	Location get_current_location() const { return current_location; }
-	Location get_destination() const { return destination; }
-};
-
-class bus{ // バス
+// バス
+class bus : public std::enable_shared_from_this<bus>
+{
 private:
 	const unsigned int ID;
 	const unsigned int capacity; // 定員
-	std::list<std::shared_ptr<passenger>> current_passengers;
-	std::vector<Location> route;
-    unsigned int current_destination_index;
-    Location current_location;
+	const std::vector<Location> route;
+	unsigned int current_passenger_num;//temp
+	Location current_location;
+	unsigned int current_destination_index;
 
 public:
-    bus(unsigned int IDnum, unsigned int cap, const std::vector<Location>& r, unsigned int current_index)
-        : ID(IDnum), capacity(cap), current_passengers(), route(r), current_destination_index((current_index + 1) % r.size()), current_location(r.at(current_index)){}
-	
-	void run()
-    {
+	bus(unsigned int IDnum, unsigned int cap, const std::vector<Location>& r, unsigned int current_index)
+		: ID(IDnum), capacity(cap), route(r), current_passenger_num(0), current_location(r.at(current_index)), current_destination_index((current_index + 1) % r.size()) {}
+
+	void run(std::unordered_multimap<Location, const bus*, pair_hash>* buses_at_busstop)
+	{
 		// バスの移動
 		if (current_location.first < route[current_destination_index].first) {
 			current_location.first++;
-		} else if (current_location.first > route[current_destination_index].first) {
+		}
+		else if (current_location.first > route[current_destination_index].first) {
 			current_location.first--;
-		} else if (current_location.second < route[current_destination_index].second) {
+		}
+		else if (current_location.second < route[current_destination_index].second) {
 			current_location.second++;
-		} else if (current_location.second > route[current_destination_index].second) {
+		}
+		else if (current_location.second > route[current_destination_index].second) {
 			current_location.second--;
 		}
 
@@ -83,49 +79,99 @@ public:
 		{
 			// 目的地を次のバス停に更新
 			current_destination_index = (current_destination_index + 1) % route.size();
+			// バス停にいるはずなので、登録
+			buses_at_busstop->insert(std::make_pair(current_location, this));
 		}
 
-		// 乗客の状態更新
-		for (auto pass_itr = current_passengers.begin(); pass_itr != current_passengers.end(); ) {
-			// バスとともに移動
-			(*pass_itr)->update_location(current_location);
-
-			// 乗客の現在地と目的地が一致していたら降車させる
-			if ((*pass_itr)->get_current_location() == (*pass_itr)->get_destination()) {
-				pass_itr = current_passengers.erase(pass_itr);//客のリストから削除
-				continue;
-			}
-
-			// eraseの関係からここでインクリメント
-			pass_itr++;
-		}
-		
 		return;
 	}
-	
-	bool is_going_to(const Location& place) {// placeにこのバスが向かうかどうか
-		bool flag = false;
-		for (unsigned int i = 0; i < route.size(); i++)
-		{
-			if (route[i] == place) { flag = true; break; }
-		}
-		return flag;
+
+	// placeにこのバスが向かうかどうか
+	bool is_going_to(const Location& place) const
+	{
+		for (unsigned int i = 0; i < route.size(); i++){ if (route[i] == place) { return true; } }
+		return false;
 	}
 
-
-	bool ride(const std::shared_ptr<passenger>& riding_passenger)
+	std::shared_ptr<const bus> ride() const
 	{
 		// capacityを超えないか判定
-		if (current_passengers.size() >= capacity) { return false; }
-
-		current_passengers.push_back(riding_passenger);
-		riding_passenger->ride();
-
-		return true;
+		if (shared_from_this().use_count() - 2 >= capacity) { return nullptr; }
+		return shared_from_this();
 	}
 
-	Location get_current_destination() const { return route[current_destination_index]; }
 	Location get_current_location() const { return current_location; }
+};
+
+class passenger { // 客
+private:
+	const unsigned int ID;
+	Location current_location; // 現在地
+	std::vector<Location> destinations;
+	std::vector<Location>::iterator dest_itr;
+	unsigned int waiting_time;
+
+	std::shared_ptr<const bus> riding_bus;
+
+public:
+	passenger(unsigned int IDnum, const Location& curr, const Location& dest, const std::unordered_map<unsigned int, std::vector<Location>>& routes)
+		: ID(IDnum), current_location(curr), destinations(GenTransferRoute(curr,dest,routes)), dest_itr(destinations.begin()), waiting_time(0)
+	{
+		// 経路構築に失敗したら
+		if (destinations.size() == 0)
+		{
+			std::cerr << "Cannot generate transfer route. (bus" << ID << ":" << current_location << "->" << dest << ")" << std::endl;
+			return;
+		}
+	}
+
+	// trueが戻り値のときはすでにゴールに達している
+	bool update(const std::unordered_multimap<Location, const bus*, pair_hash>& buses_at_busstop)
+	{
+		// バス乗車済み
+		if (riding_bus)
+		{
+			// バスに従って移動
+			if (ManhattanDistance(current_location, riding_bus->get_current_location()) > 1)// 1-ノルムで1より多く進んでいたらエラー
+			{
+				std::cerr << "fuckin' moving " << current_location << "->" << riding_bus->get_current_location() << std::endl;
+				return false;
+			}
+			current_location = riding_bus->get_current_location();
+
+			// 移動して現在の目的地なら降車
+			if (current_location == *dest_itr)
+			{
+				riding_bus = nullptr;
+				dest_itr++;	// 目的地を移動
+				// 最終目的地だったならtrueを返し通知
+				if (dest_itr == destinations.end()) { return true; }
+			}
+		}
+		else
+		{
+			// バスに乗っていないなら乗ろうとする
+			// 今いるバス停に止まっているバスを走査
+			auto itr_range = buses_at_busstop.equal_range(current_location);
+			for (auto itr = itr_range.first; itr != itr_range.second; itr++)
+			{
+				// 次の目的地に行くかチェック
+				if(itr->second->is_going_to(*dest_itr))
+				{
+					// 行くなら乗車
+					riding_bus = itr->second->ride();
+					if (riding_bus) { break; }// 乗車出来たらbreak
+				}
+			}
+
+			// 乗れなかったらwait
+			if (!riding_bus) { waiting_time++; }
+		}
+
+		return false;
+	}
+
+	unsigned int get_waiting_time() const { return waiting_time; }
 };
 
 #endif // _INC_CLASS_HEADER
--- test.cpp
+++ test.cpp
@@ -1,7 +1,6 @@
 #include "header.h"
 #include "traffic_simulator.h"
 #include <QApplication>
-#include <map>
 #include <random>
 #include <fstream>
 #include <sstream>
@@ -20,7 +19,7 @@ int main(int argc, char *argv[]){
 	setting_file >> size;	// 設定ファイルから読み込み
 
 	// バス停情報
-	std::map<unsigned int, Location> busstop_location;		// バス停位置情報
+	std::unordered_map<unsigned int, Location> busstop_location;		// バス停位置情報
 	std::vector<unsigned int> busstop_prob;	// バス停混雑情報
 	{
 		unsigned int busstop_num = 0;	// バス停総数
@@ -49,10 +48,11 @@ int main(int argc, char *argv[]){
 	}
 
 	// バス路線図
-	std::map<unsigned int, std::vector<Location>> bus_route;
+	std::unordered_map<unsigned int, std::vector<Location>> bus_route;
 	{
 		unsigned int bus_route_num = 0;	// 路線図総数
 		setting_file >> bus_route_num;	// 設定ファイルから読み込み
+		std::cout << "Bus Routes:" << std::endl;
 		for (unsigned int i = 0; i < bus_route_num; i++) {
 			unsigned int index = 0;
 			std::string route_str, tmp;
@@ -64,17 +64,11 @@ int main(int argc, char *argv[]){
 			while(std::getline(route_stream, tmp, ',')){
 				bus_route.at(index).push_back(busstop_location.at(std::atoi(tmp.c_str())));
 			}
-		}
-
-		// バス路線図の出力
-		std::cout << "Bus Routes:" << std::endl;
-		for (auto itr = bus_route.begin(); itr != bus_route.end(); itr++)
-		{
-			std::cout << (*itr).first << " : ";
-			std::cout << *((*itr).second.begin());
-			for (auto itr2 = (*itr).second.begin() + 1; itr2 != (*itr).second.end(); itr2++)
+			// バス路線図の出力
+			std::cout << index << " : " << bus_route.at(index).front();
+			for (auto itr = bus_route.at(index).begin() + 1; itr != bus_route.at(index).end(); itr++)
 			{
-				std::cout << "->" << (*itr2);
+				std::cout << "->" << *itr;
 			}
 			std::cout << std::endl;
 		}
@@ -83,108 +77,103 @@ int main(int argc, char *argv[]){
 	// 乗客配置
 	const unsigned int passenger_num = 100;	// 総乗客数
 	std::list<std::shared_ptr<passenger>> passenger_list;
-	std::random_device rnd;
-	std::uniform_int_distribution<unsigned int> rand(1, busstop_prob[busstop_prob.size() - 1]);
-	std::cout << "Passenger Distribution:" << std::endl;
-	for (unsigned int i = 0; i < passenger_num; i++)
 	{
-		// スタートと目的地のバス停をランダムに決定(バス停ごとに定められた確率に応じて)
-		unsigned int start_busstop = 0, dest_busstop = 0;
-		unsigned int rand_val = rand(rnd);
-		for (unsigned int temp = 0; temp < busstop_location.size(); temp++)
+		std::random_device rnd;
+		std::uniform_int_distribution<unsigned int> rand(1, busstop_prob[busstop_prob.size() - 1]);
+		std::cout << "Passenger Distribution:" << std::endl;
+		for (unsigned int i = 0; i < passenger_num; i++)
 		{
-			if (rand_val <= busstop_prob[temp]) { start_busstop = temp; break; }
+			// スタートと目的地のバス停をランダムに決定(バス停ごとに定められた確率に応じて)
+			unsigned int start_busstop = 0, dest_busstop = 0;
+			unsigned int rand_val = rand(rnd);
+			for (unsigned int temp = 0; temp < busstop_location.size(); temp++)
+			{
+				if (rand_val <= busstop_prob[temp]) { start_busstop = temp; break; }
+			}
+			rand_val = rand(rnd);
+			for (unsigned int temp = 0; temp < busstop_location.size(); temp++)
+			{
+				if (rand_val <= busstop_prob[temp]) { dest_busstop = temp; break; }
+			}
+			if (start_busstop == dest_busstop) { i--; continue; }	// スタートと目的地が一致したらなかったことに
+																	// 乗客を全体のリストに登録
+			passenger_list.push_back(std::make_shared<passenger>(i, busstop_location.at(start_busstop), busstop_location.at(dest_busstop), bus_route));
+			// 登録した乗客のスタート地点とゴール地点を表示
+			std::cout << i << " : " << start_busstop << busstop_location.at(start_busstop) << " -> " << dest_busstop << busstop_location.at(dest_busstop) << std::endl;
 		}
-		rand_val = rand(rnd);
-		for (unsigned int temp = 0; temp < busstop_location.size(); temp++)
-		{
-			if (rand_val <= busstop_prob[temp]) { dest_busstop = temp; break; }
-		}
-		if (start_busstop == dest_busstop) { i--; continue; }	// スタートと目的地が一致したらなかったことに
-		// 乗客を全体のリストに登録
-		passenger_list.push_back(std::make_shared<passenger>(i, busstop_location[start_busstop], busstop_location[dest_busstop]));
-		// 登録した乗客のスタート地点とゴール地点を表示
-		std::cout << i << " : " << start_busstop << busstop_location.at(start_busstop) << " -> " << dest_busstop << busstop_location.at(dest_busstop) << std::endl;
 	}
-
-	// バスの配置
+	
+	// バス配置
 	const unsigned int bus_num = 5;	// バス総台数
 	const unsigned int bus_capacity = 20; // バス定員
-	std::vector<unsigned int> bus_allocation(bus_route.size());
-	bool valid_input = false;
-	while (!valid_input)
-	{
-		std::cout << "Allocate " << bus_num << " buses among the " << bus_route.size() << " bus routes with space sparated format." << std::endl;
-		unsigned int sum = 0;
-		for (unsigned int i = 0; i < bus_allocation.size(); i++) { std::cin >> bus_allocation[i]; sum += bus_allocation[i]; }
-		if(sum == bus_num) { valid_input = true; }
-		else
-		{
-			std::cout << "The sum of allocated buses doesn't match the number of all buses." << std::endl;
-		}
-	}
-	std::cout << "Bus Distribution:" << std::endl;
 	std::list<std::shared_ptr<bus>> bus_list;
-	for (unsigned int i = 0; i < bus_allocation.size(); i++)
+	std::unordered_multimap<Location, const bus*, pair_hash> buses_at_busstop;
 	{
-		std::cout << "<Route" << i << ">" << std::endl;
-		rand = std::uniform_int_distribution<unsigned int>(0, bus_route[i].size() - 1);
-		for (unsigned int j = 0; j < bus_allocation.at(i); j++)
+		// バス路線の配分の入力
+		std::vector<unsigned int> bus_allocation(bus_route.size());
 		{
-			// スタートのバス停をランダムに決定
-			unsigned int start_busstop = rand(rnd);
-			// バスを全体のリストに登録
-			bus_list.push_back(std::make_shared<bus>(bus_list.size(), bus_capacity, bus_route[i], start_busstop));
-			// 登録したバスの初期位置表示
-			std::cout << bus_list.size() - 1 << " : " << start_busstop << bus_list.back()->get_current_location() << std::endl;
+			bool valid_input = false;
+			while (!valid_input)
+			{
+				std::cout << "Allocate " << bus_num << " buses among the " << bus_route.size() << " bus routes with space sparated format." << std::endl;
+				unsigned int sum = 0;
+				for (unsigned int i = 0; i < bus_allocation.size(); i++) { std::cin >> bus_allocation[i]; sum += bus_allocation[i]; }
+				if (sum == bus_num) { valid_input = true; }
+				else
+				{
+					std::cout << "The sum of allocated buses doesn't match the number of all buses." << std::endl;
+				}
+			}
+		}
+
+		// バス配置
+		std::random_device rnd;
+		std::cout << "Bus Distribution:" << std::endl;
+		for (unsigned int i = 0; i < bus_allocation.size(); i++)
+		{
+			std::cout << "<Route" << i << ">" << std::endl;
+			std::uniform_int_distribution<unsigned int> rand(0, static_cast<unsigned int>(bus_route[i].size() - 1));
+			for (unsigned int j = 0; j < bus_allocation.at(i); j++)
+			{
+				// スタートのバス停をランダムに決定
+				unsigned int start_busstop = rand(rnd);
+				// バスを全体のリストに登録
+				bus_list.push_back(std::make_shared<bus>(static_cast<unsigned int>(bus_list.size()), bus_capacity, bus_route[i], start_busstop));
+				buses_at_busstop.insert(std::make_pair(bus_list.back()->get_current_location(), bus_list.back().get()));
+				// 登録したバスの初期位置表示
+				std::cout << bus_list.size() - 1 << " : " << start_busstop << bus_list.back()->get_current_location() << std::endl;
+			}
 		}
 	}
 
 	// メインループ
 	std::cout << "Calculation:" << std::endl;
 	unsigned int total_waiting_time = 0;
-    while (!passenger_list.empty()) { // 乗客がいるならば
-		//人を乗せる
-		for (auto passenger_itr = passenger_list.begin(); passenger_itr != passenger_list.end(); passenger_itr++) {
-			// まだどのバスにも乗っていない乗客のみ
-			if (!(*passenger_itr)->is_riding())
-			{
-				// std::cout << "searching for passengers" << std::endl;
-				for (auto bus_itr = bus_list.begin(); bus_itr != bus_list.end(); bus_itr++) {
-					// std::cout << "searching for buses" << std::endl;
-					if ((*passenger_itr)->get_current_location() == (*bus_itr)->get_current_location() && (*bus_itr)->is_going_to((*passenger_itr)->get_destination())) {
-						// std::cout << "Someone rode a bus. Have a good trip." << std::endl;
-						if ((*bus_itr)->ride(*passenger_itr)) { break; }
-					}
-				}
-			}
-		}
-
-		// バスを進める & 乗客の降車
-		for (auto bus_itr = bus_list.begin(); bus_itr != bus_list.end(); bus_itr++) {
-			(*bus_itr)->run();
-		}
-
-		//乗れてない人のカウンタを上げる
+	// 乗客がいる限り処理
+	while (!passenger_list.empty()) {
+		// 全乗客の更新処理
 		for (auto passenger_itr = passenger_list.begin(); passenger_itr != passenger_list.end(); ) {
-			if (passenger_itr->use_count() <= 1) {	// バスに乗っていないとき
-				if ((*passenger_itr)->is_arrived()) {	// 目的地に到着したとき
-					// 全体の待ち時間を加算
-					total_waiting_time += (*passenger_itr)->get_waiting_time();
-					// 全体の客リストから除外
-					passenger_itr = passenger_list.erase(passenger_itr);
-					
-					continue;
-				} else {
-					(*passenger_itr)->waiting();	// 待ち時間を加算
-				}
-			}
+			if ((*passenger_itr)->update(buses_at_busstop))
+			{
+				// 目的地に到着した場合
+				// 全体の待ち時間を加算
+				total_waiting_time += (*passenger_itr)->get_waiting_time();
+				// 全体の客リストから除外
+				passenger_itr = passenger_list.erase(passenger_itr);
 
-			// eraseの関係からここでインクリメント
+				continue;
+			}
 			passenger_itr++;
 		}
-    }
 
+		buses_at_busstop.clear();
+
+		// バスを進める
+		for (auto bus_itr = bus_list.begin(); bus_itr != bus_list.end(); bus_itr++) {
+			(*bus_itr)->run(&buses_at_busstop);
+		}
+	}
+	
 	// 全体の待ち時間を出力
 	std::cout << "Total Waiting Time = " << total_waiting_time << std::endl;
 
